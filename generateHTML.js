814417999147d3d69bb295ae8823fccb0e814c0b
Hamza Eşsiz <webcatdev@gmail.com>
2022-07-06 12:57:04 +0300

    initialized server git

---
+++ generateHTML.js
@@ -0,0 +1,53 @@
+
+
+
+function generateHTML({fullname, language, country, age}) {
+    return `
+    <style>
+    .container {
+  padding: 1rem 2rem;
+  width: 25rem;
+  background-color: #e2edec;
+  border-radius: 3px;
+  border: 1px solid #80ddf1;
+}
+.bold {
+  font-weight: bold;
+}
+.container img {
+  width: 200px;
+}
+
+.worker-info {
+    box-sizing: border-box;
+    padding: 3rem 0;    
+    float: right;
+}
+    </style>
+    <div class="container">
+  <img src="${"https://cdn-icons-png.flaticon.com/512/64/64572.png"}" alt="worker">
+  <div class="worker-info">
+    <table>
+      <tr>
+        <td class="bold">Fullname:</td>
+        <td>${fullname}</td>
+      </tr>
+       <tr>
+        <td class="bold">Age:</td>
+        <td>${age}</td>
+      </tr>
+       <tr>
+        <td class="bold">Country:</td>
+        <td>${country}</td>
+      </tr>
+       <tr>
+        <td class="bold">Language:</td>
+        <td>${language}</td>
+      </tr>
+    </table>
+  </div>
+</div>
+  `;
+}
+
+module.exports = generateHTML
---
+++ index.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const fsPromises = require("fs").promises;
+const express = require("express");
+const app = express();
+const pdf = require("html-pdf");
+const cors = require("cors");
+const { v4: uuid } = require("uuid");
+const generateHTML = require("./generateHTML");
+
+app.use(cors());
+app.use(express.static("/image"))
+app.use(express.json());
+
+app.post("/create-pdf", async (req, res) => {
+  const id = uuid();
+  pdf
+    .create(generateHTML(req.body))
+    .toFile(path.join(__dirname, "pdf", `${id}.pdf`), (err) => {
+      if (err) res.send(Promise.reject());
+      res.send(id);
+    });
+});
+
+app.get("/fetch-pdf/:id", async (req, res) => {
+  const id = req.params.id;
+  const filePath = path.join(__dirname, "pdf", `${id}.pdf`);
+  res.setHeader("Content-Type", "application/octet-stream; charset=utf-8");
+  res.setHeader("Content-Disposition", 'attachment; filename="newPDF.pdf"');
+  res.sendFile(filePath, async () => {
+    try {
+      await fsPromises.unlink(filePath);
+    } catch (err) {
+      console.log(err);
+    }
+     
+   });
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Üzgünüz. Bu sayfa henüz oluşturulmadı.");
+});
+
+app.listen(5000);
